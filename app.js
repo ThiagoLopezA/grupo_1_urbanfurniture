72d0ecb1a79404245a8f108f8e3270a73ec33bf7
ThiagoLopezA <87744456+ThiagoLopezA@users.noreply.github.com>
2022-03-06 20:36:07 -0300

    Creación carpeta design, se agregaron imgs, creación de las dependencias y del app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// <-- Levantando el Servidor -->
+app.listen(3030, () =>
+  console.log("Servidor levantado con exito en el puerto 3030")
+);
+
+// <-- Definiendo la carpeta estatica -->
+app.use(express.static("public"));
+
+// <-- Rutas -->
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/main.html"));
+});
