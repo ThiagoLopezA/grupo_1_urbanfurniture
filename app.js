cec4c208c36849e5169103e2d7eda97b2c8fd981
Thiago López <thiagolopezalderete@hotmail.com>
2022-03-17 15:11:15 -0300

    Creación main.css (css compartido), contact.css, contact.html

--- app.js
+++ app.js
@@ -14,3 +14,6 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/main.html"));
 });
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/contact.html"));
+});
