73a033e0a87aaeff89f6bb16d8fa7a47cca4ca65
erk-art <tovarg.erika@gmail.com>
2022-03-23 22:21:43 -0300

    Subiendo html y css de carrito primera version

--- app.js
+++ app.js
@@ -21,5 +21,5 @@ app.get("/login", (req,res) => {
   res.sendFile(path.join(__dirname, "./views/login.html"))
 })
 app.get("/productCart", (req,res) => {
-  res.sendFile(path.join(__dirname, "./views/login.html"))
+  res.sendFile(path.join(__dirname, "./views/productCart.html"))
 })
