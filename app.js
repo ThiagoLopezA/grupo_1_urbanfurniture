461f79c5ed303fed49a48fd98442f2450cea85a3
joelhossein <joelhossein@protonmail.com>
2022-03-26 12:09:31 -0300

    add register.html & register.css

--- app.js
+++ app.js
@@ -17,9 +17,12 @@ app.get("/", (req, res) => {
 app.get("/contact", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/contact.html"));
 });
-app.get("/login", (req,res) => {
-  res.sendFile(path.join(__dirname, "./views/login.html"))
-})
-app.get("/productCart", (req,res) => {
-  res.sendFile(path.join(__dirname, "./views/productCart.html"))
-})
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/productCart.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
