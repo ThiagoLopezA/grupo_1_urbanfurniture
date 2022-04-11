2375060dd35fc9c847e280ca1f36d15efc88c51a
Thiago López <thiagolopezalderete@hotmail.com>
2022-04-11 14:21:30 -0300

    Inicio admin dashboard

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
-const rutaHome = require ('./src/routes/main.js');
-const userRoutes = require ('./src/routes/userRoutes.js')
-const rutaProduct = require('./src/routes/productRoutes.js')
-
-
+const rutaHome = require("./src/routes/main.js");
+const userRoutes = require("./src/routes/userRoutes.js");
+const rutaProduct = require("./src/routes/productRoutes.js");
+const adminRoutes = require("./src/routes/adminRoutes");
 
 // <-- Levantando el Servidor -->
 app.listen(3030, () =>
@@ -16,15 +15,12 @@ app.use(express.static("public"));
 
 // Motor de Plantilla EJS
 
-app.set("view engine", 'ejs');
-app.set('views', __dirname + '/src/views');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/src/views");
 app.set("views engine", "ejs");
 
-
-
 // <-- Rutas -->
-app.use('/', rutaHome);
-app.use('/users', userRoutes);
-app.use('/product', rutaProduct);
-
-
+app.use("/", rutaHome);
+app.use("/users", userRoutes);
+app.use("/product", rutaProduct);
+app.use("/admin", adminRoutes);
