29225cf93d7e93622d453f9577300b4fcf24d70c
erk-art <tovarg.erika@gmail.com>
2022-04-11 16:35:17 -0300

    modificar producto

--- src/controller/adminController.js
+++ src/controller/adminController.js
@@ -3,7 +3,33 @@ const adminController = {
         res.render("adm-dashboard/index.ejs");
     },
     modificarProducto : (req, res) => {
-        res.render("adm-dashboard/modificar.ejs");
+        let productData = [
+            {
+              src : "../img/products2.png",
+              precioAnt : 12500,
+              precio : 10000,
+              desc : "Estante"
+            },
+            {
+              src : "../img/products3.png",
+              precioAnt : 12000,
+              precio : 10000,
+              desc : "Mesa"
+            },
+            {
+              src : "../img/products4.png",
+              precioAnt : 13000,
+              precio : 10000,
+              desc : "Mueble con cajones"
+            },
+            {
+              src : "../img/products5.png",
+              precioAnt : 12500,
+              precio : 10000,
+              desc : "Mesa y juego de sillas"
+            },
+          ]
+        res.render("adm-dashboard/modificarProducto.ejs", {data: productData});
     },
     agregarProducto : (req, res) => {
         res.render("adm-dashboard/agregarProducto.ejs");
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -4,5 +4,6 @@ const adminController = require("../controller/adminController");
 
 router.get("/", adminController.index);
 router.get('/agregarProducto', adminController.agregarProducto);
+router.get('/modificarProducto', adminController.modificarProducto);
 
 module.exports = router;
