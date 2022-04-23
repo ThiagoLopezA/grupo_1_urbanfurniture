7a52f7abea6ba5e8e6d8574ad0699d3dd73e5981
Ronald <rpalacioso12@gmail.com>
2022-04-23 15:35:30 -0300

    Arreglando la data user

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,10 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const userFilePath = path.join(__dirname, "../data/user.json");
+
 const userController = {
+  getdatos: () => {return JSON.parse(fs.readFileSync(userFilePath, 'utf-8'))},
+
   login: (req, res) => {
     res.render("users/login");
   },
   register: (req, res) => {
     res.render("users/register");
   },
+  dataRegister: (req, res) =>{ 
+    let datos = userController.getdatos();
+    let response = datos.filter(user => user.email == req.body.email );
+    if (response.length == 0){
+      console.log (req.body)
+      fs.writeFileSync(userFilePath, JSON.stringify(datos, null, " "));
+      res.redirect('/')
+    }else{
+    alert('El correo esta Registrado')
+    res.redirect('/register')
+    }
+    console.log (datos),
+    res.redirect('/users/login')
+  }
 };
+
+
 
 module.exports = userController;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,5 +5,6 @@ const userController = require('../controller/userController');
 
 router.get('/login', userController.login);
 router.get('/register', userController.register);
+router.post('/login', userController.dataRegister);
 
 module.exports = router;
