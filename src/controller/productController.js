55b0d5793a436b15a7f622a1628e35a67c02b916
joelhossein <joelhossein@gmail.com>
2022-04-23 18:08:37 -0300

    se agrega CRUD de products (create y edit)

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,3 +1,4 @@
+const res = require("express/lib/response");
 const fs = require("fs");
 const path = require("path");
 const productsFilePath = path.join(__dirname, "../data/products.json");
@@ -39,6 +40,50 @@ const productController = {
     let product = data.find(product => product.id == req.params.id);
     res.render("product/product", { product: product });
   },
+  // Enviar vista de todos los productos con array de productos
+  products: (req, res) => {
+    let data = productController.getProducts();
+    res.render("product/products", { data: data });
+  },
+  // Enviar vista de crear producto
+  create: (req, res) => {
+    res.render("adm-dashboard/agregarProducto");
+  },
+  // Guardar producto nuevo en json
+  store: (req, req) => {
+    let data = productController.getProducts();
+    let newProduct = {
+      id: data.length + 1,
+      discount: req.body.discount,
+      price: req.body.price,
+      description: req.body.description,
+      name: req.body.name,
+    }
+    data.push(newProduct);
+    fs.writeFileSync(productsFilePath, JSON.stringify(data, null, 4));
+    res.redirect("product/products");
+  },
+  // Enviar vista de modificar producto
+  edit: (req, res) => {
+    let data = productController.getProducts();
+    let idProduct = req.params.id;
+    let product = data.find(product => product.id == idProduct);
+    res.render("adm-dashboard/modificarForm", { product: product });
+  },
+  // Modificar producto
+  update: (req, res) => {
+    let data = productController.getProducts();
+    let idProduct = req.params.id;
+    let product = data.find(product => product.id == idProduct);
+    product.name = req.body.name;
+    product.price = req.body.price;
+    product.discount = req.body.discount;
+    product.category = req.body.category;
+    product.description = req.body.description;
+    fs.writeFileSync(productsFilePath, JSON.stringify(data, null, 4));
+    res.redirect("product/products");
+  }
+
 };
 
 module.exports = productController;
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,6 +2,15 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controller/productController");
 
+router.get("/", productController.products);
+
+router.get("/create/", productController.create);
+router.post("create/", productController.store);
+
+router.get("/edit/:id/", productController.edit);
+router.put("/edit/:id", productController.update);
+router.delete('/delete/:id', productsController.destroy);
+
 router.get("/productCart", productController.productCart);
 router.get("/detail/:id/", productController.product);
 
