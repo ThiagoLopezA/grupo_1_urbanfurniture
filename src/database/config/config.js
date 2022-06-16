966a0a9032c5ed42cd69b612ae2255b81a4f9843
unknown <tovarg.erika@gmail.com>
2022-06-15 21:39:11 -0300

    Integrando rama erika

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: "root",
-    password: "admin",
+    password: "1258Ytovar*",
     database: "urbanfurniture",
     host: "127.0.0.1",
     dialect: "mysql",
