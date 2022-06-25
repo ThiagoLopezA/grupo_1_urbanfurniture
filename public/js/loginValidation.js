761fabbc1464be2ebe33c189103df42d76d7d3f0
Ronald <rpalacioso12@gmail.com>
2022-06-25 16:29:59 -0300

    Validacion completa del login

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -1,27 +1,35 @@
 window.addEventListener('load', function(){
 
   let formulario = document.querySelector('form');
+  let input = document.querySelectorAll('input');
+  let errorEmail = document.querySelector('.text-danger.email');
+  let errorPass  = document.querySelector('.text-danger.pass');
+  let error = 1;
   
-  formulario.addEventListener('submit', function(e){
-    e.preventDefault();
-    let input = document.querySelectorAll('input');
-    let errorEmail = document.querySelector('.text-danger.email')
-    let errorPass  = document.querySelector('.text-danger.pass')
-   
+  input[0].addEventListener('blur', e =>{
     if (input[0].value == ""){
-    errorEmail.innerText = 'No puede estar vacio este campo'
-    }else {
-      errorEmail.innerText = " ";
-    }
-
-    if (input[1].value == ""){
-      errorPass.innerText = 'No puede estar vacio este campo'
-    } else {
-      errorEmail.innerText = " ";
-   
-  } 
-    
+      errorEmail.innerText = 'No puede estar vacio este campo'
+      error = 1;
+      }else {
+        errorEmail.innerText = " ";
+        error = 0 
+      }
   });
 
+  input[1].addEventListener('blur', e =>{
+    if (input[1].value == ""){
+      errorPass.innerText = 'No puede estar vacio este campo'
+      error = 1
+    } else {
+      errorEmail.innerText = " ";
+      error = 0
+    }
+  })
+  
+  formulario.addEventListener('submit', function(e){
+    if (error == 1){
+  e.preventDefault();
+  }
+});
 
 })
