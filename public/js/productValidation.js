2fdd9a6599cb6d8e7a9775f5d47d865ddee7d838
erikaTovar <tovarg.erika@gmail.com>
2022-07-04 18:24:22 -0300

    agregando errores

--- public/js/productValidation.js
+++ public/js/productValidation.js
@@ -3,14 +3,20 @@ window.addEventListener("load", ()=>{
     let price = document.querySelector('#price');
     let discount = document.querySelector('#discount');
     let description = document.querySelector('#description');
-    let errorName = document.querySelector('text.danger.name');
-    let errorPrecio = document.querySelector('text.danger.price');
-    let errorDiscount = document.querySelector('text.danger.discount');
-    let errorDescription = document.querySelector('text.danger.description');
+    let errorName = document.querySelector('.text-danger.name');
+    let errorPrecio = document.querySelector('.text-danger.price');
+    let errorDiscount = document.querySelector('.text-danger.discount');
+    let errorDescription = document.querySelector('.text-danger.description');
+
+
+    name.addEventListener('blur',(e)=>preventEmpty(e, errorName));
+    price.addEventListener('blur',(e)=>preventEmpty(e, errorPrecio));
+    discount.addEventListener('blur',(e)=>preventEmpty(e,errorDiscount));
+    description.addEventListener('blur',(e)=>preventEmpty(e, errorDescription));
 
     function preventEmpty(event, element) {
         if (!event.target.value.length > 0) {
-          element.innerText = "Obligatorio rellenar este campo";
+          element.innerText = "Campo vacio";
           return false;
         } else {
           element.innerText = "";
