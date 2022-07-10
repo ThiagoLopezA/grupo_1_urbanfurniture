24b8ed8b351d820da8c4723eacc17d695a12cdcb
Ronald <rpalacioso12@gmail.com>
2022-07-10 13:39:01 -0300

    feat(api): Create update & delete user done

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -30,6 +30,41 @@ module.exports = {
   },
   createUser: async (req, res) => {
     try {
+      /*
+      let fields = [
+        'email',
+        'password',
+        'access',
+        'phone',
+        'dni',
+        'street',
+        'street_number',
+        'floor',
+        'apartment',
+        'province',
+        'town',
+        'CP'
+      ];
+      let errors = [];
+      fields.forEach(field => {
+        if (!req.body.hasOwnProperty(field)) {
+          errors.push(field);
+        }
+      });
+      if (errors.length > 0) {
+        res.status(400).json({
+          status: 400,
+          message: "Los siguientes campos estan incompletos",
+          fields: errors,
+        });
+      } else {
+        */
+      await db.User.create(req.body);
+      res.status(200).json({
+        status:200,
+        message:"Se creo correctamente el Usuario"
+      })
+    //}
     } catch (e) {
       res.status(500).json({
         message: "Ocurrio un error:",
@@ -39,6 +74,11 @@ module.exports = {
   },
   updateUser: async (req, res) => {
     try {
+      await db.User.update(req.body, { where: { idusers: req.params.id } })
+      res.status(200).json({
+        status: 200,
+        message: "Usuario actualizado correcto"
+      })
     } catch (e) {
       res.status(500).json({
         message: "Ocurrio un error:",
@@ -48,6 +88,11 @@ module.exports = {
   },
   deleteUser: async (req, res) => {
     try {
+     await db.User.destroy({ where: { idusers: req.params.id } })
+     res.status(200).json({
+      status: 200,
+      message: "Usuario elimado correctamente"
+    })
     } catch (e) {
       res.status(500).json({
         message: "Ocurrio un error:",
